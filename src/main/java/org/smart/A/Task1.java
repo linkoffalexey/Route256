13117d9585253384084c0846dd463f26eabefdbe
linkoffalexey <linkoffalexey@gmail.com>
2023-08-14 21:39:02 +0300

    256

---
+++ src/main/java/org/smart/A/Task1.java
@@ -0,0 +1,20 @@
+package org.smart.A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task1 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int testCaseCount = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < testCaseCount; i++) {
+                String[] lineItems = reader.readLine().split(" ");
+                int a = Integer.parseInt(lineItems[0]);
+                int b = Integer.parseInt(lineItems[1]);
+                System.out.println(a + b);
+            }
+        }
+    }
+
+
